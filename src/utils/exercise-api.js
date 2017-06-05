2d7dd9e1ff17bff2e24959ba749d457d490142b5
jim-moody <jim.moody@libertymutual.com>
2017-06-05 10:02:11 -0400

    Add the full api for exercises
    This interacts with the exercise api and performs all CRUD actions

---
+++ src/utils/exercise-api.js
@@ -0,0 +1,63 @@
+import $ from 'jquery'
+import Auth from '../modules/Auth'
+
+const url = process.env.REACT_APP_API_URL
+
+const createExercise = (text) =>
+  $.ajax({
+    url: url + '/exercises',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + Auth.getToken()
+    },
+    data: {
+      exercise: {
+        text
+      }
+    }
+  })
+
+const index = () =>
+$.ajax({
+  url: url + '/exercises',
+  method: 'GET',
+  headers: {
+    Authorization: 'Token token=' + Auth.getToken()
+  }
+})
+
+const show = (id) =>
+  $.ajax({
+    url: url + '/exercises/' + id,
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + Auth.getToken()
+    }
+  })
+
+const update = (id, exercise) =>
+  $.ajax({
+    url: url + '/exercises/' + id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + Auth.getToken()
+    },
+    data: { exercise }
+  })
+
+const destroy = id =>
+  $.ajax({
+    url: url + '/exercises/' + id,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + Auth.getToken()
+    }
+  })
+
+export {
+  createExercise,
+  index,
+  show,
+  update,
+  destroy
+}

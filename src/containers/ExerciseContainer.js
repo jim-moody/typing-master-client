ba375b7779bda5e3a28c45e22b8b6e16568dccba
jim-moody <jim.moody@libertymutual.com>
2017-06-07 12:43:56 -0400

    Add tabs to the exercise view
    This will allow us to separate the leaderboard of top scores from the actual typing area

--- src/containers/ExerciseContainer.js
+++ src/containers/ExerciseContainer.js
@@ -3,26 +3,40 @@ import PropTypes from 'prop-types'
 import { show } from '../utils/exercise-api'
 import TypingArea from './TypingArea'
 import Paper from 'material-ui/Paper'
+import TopScores from '../components/TopScores'
+import {Tabs, Tab} from 'material-ui/Tabs'
+import {teal700} from 'material-ui/styles/colors'
 
 class ExerciseContainer extends Component {
   constructor (props) {
     super(props)
     this.state = {
       text: '',
-      id: ''
+      id: '',
+      scores: []
     }
   }
 
   componentDidMount () {
     show(this.props.match.params.id)
-      .then(res => this.setState({text: res.exercise.text}))
+      .then(res => this.setState({text: res.exercise.text, scores: res.exercise.scores}))
       .catch(console.error)
   }
   render () {
     const props = this.props
 
     return (
+      <Tabs tabItemContainerStyle={{backgroundColor: teal700}}  >
+        <Tab label='Exercise'>
         <TypingArea text={this.state.text} exerciseId={this.props.match.params.id} {...props} />
+          </Tab>
+          <Tab label='Leaderboard'>
+          <h2 style={{textAlign: 'center'}}>Leaderboard</h2>
+        <Paper style={{maxWidth: '700px', margin: '20px auto', overflow: 'hidden'}}>
+        <TopScores scores={this.state.scores}/>
+        </Paper>
+        </Tab>
+      </Tabs>
     )
   }
 }
